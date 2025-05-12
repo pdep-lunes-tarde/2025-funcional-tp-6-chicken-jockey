1dd66feef286c1e3419eea22a1cb1d7d2a0ff2c9
piccolit <tpiccoli@frba.utn.edu.ar>
2025-05-12 00:43:21 -0300

    terminado

--- src/Library.hs
+++ src/Library.hs
@@ -3,16 +3,22 @@ import PdePreludat
 import Data.Bool (Bool)
 
 data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras | Papas
+    Carne | Pan | Panceta | Cheddar | Pollo | Curry |
+    QuesoDeAlmendras | Papas | BaconDeTofu | PatiVegano | PanIntegral
     deriving (Eq, Show)
+
 
 precioIngrediente Carne = 20
 precioIngrediente Pan = 2
 precioIngrediente Panceta = 10
 precioIngrediente Cheddar = 10
-precioIngrediente Pollo =  10
+precioIngrediente Pollo = 10
 precioIngrediente Curry = 5
 precioIngrediente QuesoDeAlmendras = 15
+precioIngrediente BaconDeTofu = 12
+precioIngrediente PatiVegano = 10
+precioIngrediente PanIntegral = 3
+precioIngrediente Papas = 10
 
 data Hamburguesa = Hamburguesa {
     precioBase :: Number,
@@ -29,30 +35,67 @@ esCarne ingrediente = ingrediente == Carne
 esPollo :: Ingrediente -> Bool
 esPollo ingrediente = ingrediente == Pollo
 
+esVegano :: Ingrediente -> Bool
+esVegano ingrediente = ingrediente == PatiVegano
+
 agrandar :: Hamburguesa -> Hamburguesa
 agrandar (Hamburguesa precioBase ingredientes)
     | any esCarne ingredientes = Hamburguesa precioBase (Carne : ingredientes)
     | any esPollo ingredientes = Hamburguesa precioBase (Pollo : ingredientes)
+    | any esVegano ingredientes = Hamburguesa precioBase (PatiVegano : ingredientes)
     | otherwise = Hamburguesa precioBase ingredientes
 
 agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
-agregarIngrediente ingredienteAgregado hamburguesa = hamburguesa {ingredientes = ingredienteAgregado : ingredientes}
+agregarIngrediente ingredienteAgregado hamburguesa = hamburguesa {ingredientes = ingredienteAgregado : ingredientes hamburguesa}
 
 descuento :: Number -> Hamburguesa -> Hamburguesa
 descuento porcentaje (Hamburguesa precioBase ingredientes) = Hamburguesa (precioBase * (1 - porcentaje / 100)) ingredientes
 
 pdepBurger :: Hamburguesa
-pdepBurger = descuento 20 . agregarIngrediente Panceta . agregarIngrediente Cheddar . agrandar . agrandar cuartoDeLibra
+pdepBurger = descuento 20 . agregarIngrediente Panceta . agregarIngrediente Cheddar . agrandar . agrandar $ cuartoDeLibra
+
 
 -- Punto 2
 
 dobleCuarto :: Hamburguesa
-dobleCuarto = agregarIngrediente Cheddar . agrandar cuartoDeLibra
+dobleCuarto = agregarIngrediente Cheddar . agrandar $ cuartoDeLibra
 
 bigPdep :: Hamburguesa
 bigPdep = agregarIngrediente Curry dobleCuarto
 
-precioIngrediente Papas = 10
-
 delDia :: Hamburguesa -> Hamburguesa
-delDia hamburguesa = descuento 30 . agregarIngrediente Papas hamburguesa
+delDia = descuento 30 . agregarIngrediente Papas
+
+-- Punto 3
+
+modificarIngredientes :: ([Ingrediente] -> [Ingrediente]) -> Hamburguesa -> Hamburguesa
+modificarIngredientes f hamburguesa = hamburguesa { ingredientes = f (ingredientes hamburguesa) }
+
+modificarPrecioBase :: (Number -> Number) -> Hamburguesa -> Hamburguesa
+modificarPrecioBase f hamburguesa = hamburguesa { precioBase = f (precioBase hamburguesa) }
+
+hacerVeggie :: Hamburguesa -> Hamburguesa
+hacerVeggie = modificarIngredientes (map reemplazoVegano)
+
+reemplazoVegano :: Ingrediente -> Ingrediente
+reemplazoVegano = cambio Carne PatiVegano . cambio Pollo PatiVegano . cambio Cheddar QuesoDeAlmendras . cambio Panceta BaconDeTofu
+
+cambiarPanDePati :: Hamburguesa -> Hamburguesa
+cambiarPanDePati = modificarIngredientes (map reemplazoPan)
+
+reemplazoPan :: Ingrediente -> Ingrediente
+reemplazoPan = cambio Pan PanIntegral
+
+cambio :: Eq a => a -> a -> a -> a
+cambio original nuevo actual
+    | actual == original = nuevo
+    | otherwise = actual
+
+
+dobleCuartoVegano :: Hamburguesa
+dobleCuartoVegano = cambiarPanDePati . agrandar . hacerVeggie $ dobleCuarto
+
+-- Utilidad: precio total
+
+precioTotal :: Hamburguesa -> Number
+precioTotal hamburguesa = precioBase hamburguesa + sum (map precioIngrediente (ingredientes hamburguesa))
--- src/Spec.hs
+++ src/Spec.hs
@@ -1,4 +1,5 @@
 module Spec where
+
 import PdePreludat
 import Library
 import Test.Hspec
@@ -6,6 +7,38 @@ import Control.Exception (evaluate)
 
 correrTests :: IO ()
 correrTests = hspec $ do
-    describe "TP 5" $ do
-        it "test de prueba" $ do
-            2 + 2 `shouldBe` 4
+    describe "TP 5 - Hamburguesas" $ do
+
+        it "El cuarto de libra tiene precio total 54" $ do
+            precioTotal cuartoDeLibra `shouldBe` 54
+            -- base 20 + (2 + 20 + 10 + 2)
+
+        it "La pdepBurger tiene precio total 110" $ do
+            precioTotal pdepBurger `shouldBe` 110
+            -- base 20 * 0.8 = 16 + ingredientes suman 94
+
+        it "El dobleCuarto tiene precio total 84" $ do
+            precioTotal dobleCuarto `shouldBe` 84
+
+        it "El bigPdep tiene precio total 89" $ do
+            precioTotal bigPdep `shouldBe` 89
+
+        it "Una dobleCuarto del día tiene precio total 88" $ do
+            precioTotal (delDia dobleCuarto) `shouldBe` 88
+
+        it "hacerVeggie reemplaza correctamente los ingredientes base" $ do
+            let hamburguesa = Hamburguesa 0 [Carne, Pollo, Cheddar, Panceta, Pan]
+            ingredientes (hacerVeggie hamburguesa)
+                `shouldBe` [PatiVegano, PatiVegano, QuesoDeAlmendras, BaconDeTofu, Pan]
+
+        it "cambiarPanDePati reemplaza los panes por pan integral" $ do
+            let hamburguesa = Hamburguesa 0 [Pan, Carne, Pan]
+            ingredientes (cambiarPanDePati hamburguesa)
+                `shouldBe` [PanIntegral, Carne, PanIntegral]
+
+        it "El dobleCuartoVegano tiene precio total 86" $ do
+            precioTotal dobleCuartoVegano `shouldBe` 86
+
+        it "Agrandar una hamburguesa veggie agrega otro PatiVegano" $ do
+            let hamb = Hamburguesa 0 [PatiVegano]
+            ingredientes (agrandar hamb) `shouldBe` [PatiVegano, PatiVegano]
